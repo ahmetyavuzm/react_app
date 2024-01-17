d70b9f9b78978bff409676e8816f3aad8edceadf
ahmetyavuzm <ahmetyavuzm@gmail.com>
2024-01-18 02:10:02 +0300

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
 
-  const fetchComments = async () => {
-    const res = await fetch('http://127.0.0.1:5000/get_links')
-    const json = await res.json()
-    console.log(json)
-  }
-
-  fetchComments();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
