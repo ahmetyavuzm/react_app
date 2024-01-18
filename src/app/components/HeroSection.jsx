12a3c6656fd26ec99b6835a3e7e4e5d43af01ff0
ahmetyavuzm <ahmetyavuzm@gmail.com>
2024-01-18 12:52:02 +0300

    changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12 ">
         <div className="col-span-7 place-self-center">
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I'am Yavuz
+            {"Hello, I'am Yavuz"}
           </h1>
           <p className="text-[#ADB7BE] text-lg lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
